376505e1e9ad974b8b026c868f50c624b4722911
SRIHAAS-PIGILAM <srihaas.pigilam@gmail.com>
2023-08-11 00:12:14 +0530

    Python Practice

---
+++ 12 DAY-12/Number guessing game/final.py
@@ -0,0 +1,90 @@
+# Number Guessing Game Objectives:
+
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player.
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+import random
+import art
+import os
+# Clearing the Screen
+os.system('cls')
+print(art.logo)
+
+print("Welcome to the Number Guessing game!\U0001F60E")
+print("UMMMM!!!!! Thinking of a number between 1 and 100.!!!\U0001F971")
+
+# generating a random number to guess
+guess_number = random.randint(1, 101)
+print(guess_number)
+difficulty = input(
+    f"Choose the difficulty level\U0001F914 , Type 'easy'  or 'hard' ").lower()
+oho = ""
+if difficulty == "easy":
+    guess = 10
+    print(
+        f"You have {guess} attempts to guess the number correctly\U0001F920.")
+    initial_guess = int(input("make a guess: "))
+
+    while (guess != 0):
+        if guess_number > initial_guess:
+            print("Too High!!")
+            print("guess again")
+            guess -= 1
+            print(
+                f"You have {guess} attempts to guess the number correctly\U0001F920.")
+            guess_again = int(input("guess:"))
+            initial_guess = guess_again
+            oho = "All guess are completed you lost!!!"
+        elif guess_number < initial_guess:
+            print("too low")
+            print("guess again")
+            guess -= 1
+            print(
+                f"You have {guess} attempts to guess the number correctly\U0001F920.")
+            guess_again = int(input("guess:"))
+            initial_guess = guess_again
+            oho = "All guess are completed you lost!!!"
+        elif guess_number-initial_guess == 0:
+            oho = "Hoorraahh!!! You guessed it correct"
+            guess = 0
+            art.lost = art.won
+
+    print(f"Hahaa {oho}")
+    print(art.lost)
+
+else:
+    guess = 5
+    print(
+        f"You have {guess} attempts remaining to guess the number correctly\U0001F62C")
+    initial_guess = int(input("make a guess: "))
+
+    while (guess != 0):
+        if guess_number > initial_guess:
+            print("Too High!!")
+            print("guess again")
+            guess -= 1
+            print(
+                f"You have {guess} attempts to guess the number correctly\U0001F920.")
+            guess_again = int(input("guess:"))
+            initial_guess = guess_again
+            oho = "All guess are completed you lost!!!"
+        elif guess_number < initial_guess:
+            print("too low")
+            print("guess again")
+            guess -= 1
+            print(
+                f"You have {guess} attempts to guess the number correctly\U0001F920.")
+            guess_again = int(input("guess:"))
+            initial_guess = guess_again
+            oho = "All guess are completed you lost!!!"
+        elif guess_number-initial_guess == 0:
+            oho = "Hoorraahh!!! You guessed it correct"
+            guess = 0
+            art.lost = art.won
+
+    print(f"{oho}")
+    print(art.lost)
